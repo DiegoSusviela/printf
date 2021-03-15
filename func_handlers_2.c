51763571f12142179d33a42bf70128f8acbf8e1d
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-15 09:49:57 -0700

    changed to _strlen

--- func_handlers_2.c
+++ func_handlers_2.c
@@ -18,3 +18,20 @@ int print_number_uns(unsigned int n)
 
 	return (num_lenght_uns(n));
 }
+
+/**
+ * _strlen - Entry point
+ * @s: wopa
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int marker = 0;
+
+	while (*(s + marker))
+		marker++;
+	return (marker);
+}
