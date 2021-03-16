53f3d94d6077b6448f7ff4abbbcb80b01d09a956
elyoguictm <2788@holbertonschool.com>
2021-03-16 13:06:11 +0000

    repair betty

--- holberton.h
+++ holberton.h
@@ -1,62 +1,76 @@
-#ifndef prinf_function
-#define prinf_function
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <limits.h>
-#include <stdio.h>
-#include <math.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include <stdarg.h>
-#include <unistd.h>
-
-int _printf(const char *format, ...);
-
-void print_str(int pos1, int pos2, const char *format);
-void _puts(char *str);
-int print_number(int n);
-char *change_to_binary(unsigned int k);
-
-int print_number_uns(unsigned int n);
-int _strlen(char *s);
-void _print_rev_recursion(char *s);
-
-int print_char(va_list list);
-int print_char_2(va_list list);
-int print_int(va_list list);
-int print_to_binar(va_list list);
-int print_percent(va_list list);
-
-int print_uns(va_list list);
-int print_octal(va_list list);
-int print_hexa_low(va_list list);
-int print_hexa_upper(va_list list);
-int print_str_non_print(va_list list);
-
-int print_str_rev(va_list list);
-int print_rot_13(va_list list);
-int print_p(va_list list);
-
-
-int _putchar(char);
-int num_lenght(int n);
-int num_lenght_uns(unsigned int n);
 
 /**
- * struct datatype - imprime
- * @type: numero a imprimir
- * @func: un char ahi
- *
-* Description: Show a message blablabla
-* Return: Always 0 (Success)
-*/
-
-typedef struct datatype
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
 {
-	char *type;
-	int (*func)(va_list list);
-} data_t;
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
 
 #endif
