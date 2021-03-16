211925d7ae0426f35f88c0a6cf44fcc240f842f4
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-16 11:51:43 -0300

    testing new logic

--- my_main_function.c
+++ my_main_function.c
@@ -6,7 +6,7 @@
  *
  * Return: integer.
  */
-
+/*
 int _printf(const char *format, ...)
 {
 	data_t type[] = {
@@ -20,8 +20,6 @@ int _printf(const char *format, ...)
 	};
 
 	int indx_type, iter = 0, count = 0;
-	/* int pos1 = 0, pos2; */
-	/*int (*f)(va_list);*/
 
 	va_list list;
 
@@ -32,10 +30,6 @@ int _printf(const char *format, ...)
 
 	while (format[iter])
 	{
-		/*
-		if (format[iter] == '%' && format[iter + 1] == '\0')
-			return (-1);
-		*/
 		while (format[iter] && format[iter] != '%')
 		{
 			_putchar(format[iter]);
@@ -44,40 +38,88 @@ int _printf(const char *format, ...)
 		}		
 		if (format[iter])
 		{
-			/*if (format[iter] && format[iter + 1] == '\0')
+			indx_type = 0;
+			iter++;
+			while (type[indx_type].type)
 			{
-				_putchar('%');
-				count++;
+				if (*type[indx_type].type == format[iter])
+					count += type[indx_type].func(list);
+				indx_type++;
 			}
-			else
-			{*/
-				indx_type = 0;
-				/*iter++;*/
-				while (type[indx_type].type)
-				{
-					if (*type[indx_type].type == format[iter + 1])
-					{
-						if (*type[indx_type].type)
-							count += type[indx_type].func(list);
-						break;
-					}
-					indx_type++;
-				}
-				if (!format[iter + 1])
-					return (-1);
-				if (!type[indx_type].type)
-				{
-					_putchar(format[iter]);
-					count++;
-					iter++;
-				}
-				else
-					iter++;
-			/*}*/
 		}
 		else
 			break;
+		iter++;
 	}
 	va_end(list);
+	return (count);
+}*/
+
+
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	data_t type[] = {
+		{"c", print_char}, {"s", print_char_2},
+		{"d", print_int}, {"i", print_int},
+		{"b", print_to_binar}, {"%", print_percent},
+		{"u", print_uns}, {"o", print_octal},
+		{"x", print_hexa_low}, {"X", print_hexa_upper},
+		{"S", print_str_non_print}, {"r", print_str_rev},
+		{"R", print_rot_13}, {"p", print_p}, {NULL, NULL},
+	};
+
+	for (i = 0; type[i].type != NULL; i++)
+	{
+		if (*(type[i].type) == *format)
+		{
+			break;
+		}
+	}
+	return (type[i].func);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
 	return (count);
 }
