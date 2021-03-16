1f3dc5896aedd59037a96793faa6e1fc0f20ce20
Gabriel Ferreira <73730905+elyoguictm@users.noreply.github.com>
2021-03-16 11:00:15 -0300

    repair

--- aux_funcs.c
+++ aux_funcs.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,23 +6,19 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
 /**
  * num_lenght - counts number of digits of a number
  * @n: string to count digits.
  *
  * Return: digit quantity for count.
  */
-
 int num_lenght(int n)
 {
 	int counter = 1;
-
 	if (n < 0)
 		counter++;
 	while (n / 10 != 0)
@@ -33,7 +28,6 @@ int num_lenght(int n)
 	}
 	return (counter);
 }
-
 /**
  * num_lenght_uns - counts number of digits of a number
  * @n: string to count digits.
@@ -43,7 +37,6 @@ int num_lenght(int n)
 int num_lenght_uns(unsigned int n)
 {
 	int counter = 1;
-
 	while (n / 10 != 0)
 	{
 		counter++;
